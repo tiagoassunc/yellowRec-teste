f08a508aefa3c5fdd0282c5d87a6961851f521d0
Tiago Assuncao <tiagoassunc@gmail.com>
2022-10-26 09:05:13 -0300

    Update Explore component

--- src/components/Explore/Explore.js
+++ src/components/Explore/Explore.js
@@ -7,7 +7,12 @@ import { CandidatesList } from "../CandidatesList/CandidatesList";
 export const Explore = () => {
   return (
     <div className="explore-container">
-      <div className="list-title">Recruiting can be easier</div>
+      <div className="yellowRec-container">
+        <div className="list-title">Recruiting can be easier!</div>
+        <div className="list-description">
+          💛 Recruit quickly and right with Yellow.
+        </div>
+      </div>
       <CandidatesList />
     </div>
   );
