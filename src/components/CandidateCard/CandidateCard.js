60e88464ae94e9ef10b13e0f9e4e2c33bb45e7c6
Tiago Assuncao <tiagoassunc@gmail.com>
2022-10-26 07:44:32 -0300

    Update CandidateCard component

--- src/components/CandidateCard/CandidateCard.js
+++ src/components/CandidateCard/CandidateCard.js
@@ -1,9 +1,31 @@
 import React from "react";
 
+import { FaExpand, FaRegSmileBeam } from "react-icons/fa";
+
 import "./CandidateCard.styles.scss";
 
 export const CandidateCard = ({ candidate }) => {
   console.log("candidate", candidate);
 
-  return <div className="candidateCard-container">{candidate.name}</div>;
+  return (
+    <div className="candidateCard-container">
+      <div className="moreInfo-container">
+        <div className="buttonMore-info">
+          <FaExpand />
+        </div>
+      </div>
+      <div className="mainContent-container">
+        <div className="photo-container">
+          <div className="buttonSmile-photo">
+            <FaRegSmileBeam
+              style={{
+                color: "black",
+                fontSize: "2rem",
+              }}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
