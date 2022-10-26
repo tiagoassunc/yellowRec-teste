cdaf55a90ee3255f20eb4d3badc2e5dab1e8f5ff
Tiago Assuncao <tiagoassunc@gmail.com>
2022-10-26 11:26:22 -0300

    Update Footer component

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Footer.styles.scss";
+
+export const Footer = () => {
+  return (
+    <div className="footer-container">
+      <p className="footer-title">Tiago Assunção B.</p>
+    </div>
+  );
+};
