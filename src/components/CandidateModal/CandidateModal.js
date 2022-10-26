b8fe14e3eafd54a533e0cf67a6b85791ea721c54
Tiago Assuncao <tiagoassunc@gmail.com>
2022-10-26 11:14:07 -0300

    Update CandidateModal component

--- src/components/CandidateModal/CandidateModal.js
+++ src/components/CandidateModal/CandidateModal.js
@@ -1,16 +1,108 @@
-import React from "react";
-import { useSelector } from "react-redux";
+import React, { useState } from "react";
+
+import { FaRegTimesCircle } from "react-icons/fa";
 
 import "./CandidateModal.styles.scss";
 
-export const CandidateModal = (candidateId) => {
-  const list = useSelector((state) => state.candidatesList);
+export const CandidateModal = ({ candidate, closeModal }) => {
+  const [activeType, setActiveType] = useState("candidate");
 
-  console.log("candidateId", candidateId);
+  const getCandidateInfo = () => {
+    return (
+      <div className="completeInfo">
+        <div className="section-CompleteInfo">
+          <p className="completeInfo-container-title">Name:</p>
+          <p className="completeInfo-container-content">{candidate.name}</p>
+        </div>
+        <div className="section-CompleteInfo">
+          <p className="completeInfo-container-title">Email:</p>
+          <p className="completeInfo-container-content">{candidate.email}</p>
+        </div>
+        <div className="section-CompleteInfo">
+          <p className="completeInfo-container-title">User name:</p>
+          <p className="completeInfo-container-content">{candidate.username}</p>
+        </div>
+        <div className="section-CompleteInfo">
+          <p className="completeInfo-container-title">Phone</p>
+          <p className="completeInfo-container-content">{candidate.phone}</p>
+        </div>
+        <div className="section-CompleteInfo">
+          <p className="completeInfo-container-title">City</p>
+          <p className="completeInfo-container-content">
+            {candidate.address.city}
+          </p>
+        </div>
+        <div className="section-CompleteInfo">
+          <p className="completeInfo-container-title">Website</p>
+          <p className="completeInfo-container-content">{candidate.website}</p>
+        </div>
+      </div>
+    );
+  };
+
+  const getCompanyInfo = () => {
+    return (
+      <div className="completeInfo">
+        <div className="section-CompleteInfo">
+          <p className="completeInfo-container-title">Company Name:</p>
+          <p className="completeInfo-container-content">
+            {candidate.company.name}
+          </p>
+        </div>
+        <div className="section-CompleteInfo">
+          <p className="completeInfo-container-title">Company Atuation:</p>
+          <p className="completeInfo-container-content">
+            {candidate.company.bs}
+          </p>
+        </div>
+        <div className="section-CompleteInfo">
+          <p className="completeInfo-container-title">More About:</p>
+          <p className="completeInfo-container-content">
+            {candidate.company.catchPhrase}
+          </p>
+        </div>
+      </div>
+    );
+  };
 
   return (
     <div className="modal-container">
-      <div className="candidate-modal"> teste</div>
+      <div className="candidate-modal">
+        <div className="type-handler">
+          <button
+            className="type-handler-bttn"
+            onClick={() => {
+              setActiveType("candidate");
+            }}
+          >
+            👩🏾Candidate
+          </button>
+          <button
+            className="type-handler-bttn"
+            onClick={() => {
+              setActiveType("company");
+            }}
+          >
+            🏢Company
+          </button>
+        </div>
+        {activeType === "candidate" ? getCandidateInfo() : getCompanyInfo()}
+        <div className="candidate-modal-close">
+          <button
+            className="close-container"
+            onClick={() => {
+              closeModal();
+            }}
+          >
+            <FaRegTimesCircle
+              style={{
+                color: "#fad502",
+                fontSize: "3.25rem",
+              }}
+            />
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
