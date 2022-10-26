f64752cdeabdbeef8e0d4e90d13afae5bb98c1a3
Tiago Assuncao <tiagoassunc@gmail.com>
2022-10-26 00:32:20 -0300

    Create Explore component

---
+++ src/components/Explore/Explore.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import "./Explore.styles.scss";
+
+export const Explore = () => {
+  return (
+    <div className="explore-container">
+      <div className="list-title">Recruiting can be easier</div>
+    </div>
+  );
+};
