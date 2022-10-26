7e756e1558285cc81c040d3426c2b642c8cee320
Tiago Assuncao <tiagoassunc@gmail.com>
2022-10-26 02:06:00 -0300

    Update CandidatesList component

--- src/components/CandidatesList/CandidatesList.js
+++ src/components/CandidatesList/CandidatesList.js
@@ -1,7 +1,20 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 import "./CandidatesList.styles.scss";
 
+import { CandidateCard } from "../CandidateCard/CandidateCard";
+
 export const CandidatesList = () => {
-  return <div className="candidatesList-container"></div>;
+  const list = useSelector((state) => state.candidatesList);
+
+  console.log("list", list.candidates);
+
+  return (
+    <div className="candidatesList-container">
+      {list.candidates.map((currentCandidate) => (
+        <CandidateCard candidate={currentCandidate}></CandidateCard>
+      ))}
+    </div>
+  );
 };
