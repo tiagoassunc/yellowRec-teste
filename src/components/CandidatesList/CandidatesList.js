0aa8bc3b957a44ed6702760b4b62da2665ae64b7
Tiago Assuncao <tiagoassunc@gmail.com>
2022-10-26 11:58:52 -0300

    Add key to components render map

--- src/components/CandidatesList/CandidatesList.js
+++ src/components/CandidatesList/CandidatesList.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import { v4 as uuidv4 } from "uuid";
 
 import "./CandidatesList.styles.scss";
 
@@ -8,10 +9,15 @@ import { CandidateCard } from "../CandidateCard/CandidateCard";
 export const CandidatesList = () => {
   const list = useSelector((state) => state.candidatesList);
 
+  const { v4: uuid } = require("uuid");
+
   return (
     <div className="candidatesList-container">
       {list.candidates.map((currentCandidate) => (
-        <CandidateCard candidate={currentCandidate}></CandidateCard>
+        <CandidateCard
+          key={uuid()}
+          candidate={currentCandidate}
+        ></CandidateCard>
       ))}
     </div>
   );
