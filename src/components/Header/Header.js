3b46a8dc356db003faa9e18c8e1712c779b5b162
Tiago Assuncao <tiagoassunc@gmail.com>
2022-10-25 22:56:29 -0300

    Create Header component

---
+++ src/components/Header/Header.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Header.styles.scss";
+
+export const Header = () => {
+  return (
+    <div className="header-container">
+      <p className="logo-title">YW.</p>
+    </div>
+  );
+};
