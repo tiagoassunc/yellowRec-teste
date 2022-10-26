db0fd44bb72f4a88630e978c82c71e2012bfc9df
Tiago Assuncao <tiagoassunc@gmail.com>
2022-10-26 01:09:21 -0300

    Create CandidatesList component

---
+++ src/components/CandidatesList/CandidatesList.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+import "./CandidatesList.styles.scss";
+
+export const CandidatesList = () => {
+  return <div className="candidatesList-container"></div>;
+};
--- src/components/Explore/Explore.js
+++ src/components/Explore/Explore.js
@@ -2,10 +2,13 @@ import React from "react";
 
 import "./Explore.styles.scss";
 
+import { CandidatesList } from "../CandidatesList/CandidatesList";
+
 export const Explore = () => {
   return (
     <div className="explore-container">
       <div className="list-title">Recruiting can be easier</div>
+      <CandidatesList />
     </div>
   );
 };
